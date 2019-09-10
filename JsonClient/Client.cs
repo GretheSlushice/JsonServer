7720245c5d6ae14f56adc2ec5983b1a555632395
Grethe Slushice <simonshn.gmh@gmail.com>
2019-09-10 13:10:55 +0200

    Initial commit

---
+++ JsonClient/Client.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Net.Sockets;
+using ModelLib;
+using Newtonsoft.Json;
+
+namespace JsonClient
+{
+    public class Client
+    {
+        public void Start()
+        {
+            Car myCar = new Car("Tesla", "Green", "JsonCar4");
+
+            using (TcpClient socket = new TcpClient("localhost", 10002))
+            using (StreamWriter sw = new StreamWriter(socket.GetStream()))
+            using (StreamReader sr = new StreamReader(socket.GetStream()))
+            {
+                string output = JsonConvert.SerializeObject(myCar);
+                sw.WriteLine(output);
+                sw.Flush();
+            }
+        }
+    }
+}
---
+++ JsonServer/Server.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using ModelLib;
+using Newtonsoft.Json;
+
+namespace JsonServer
+{
+    public class Server
+    {
+        public void Start()
+        {
+            TcpListener server = new TcpListener(IPAddress.Loopback, 10002);
+            server.Start();
+
+            while (true)
+            {
+                TcpClient socket = server.AcceptTcpClient();
+                Task.Run(() =>
+                {
+                    TcpClient tmpSocket = socket;
+                    DoClient(tmpSocket);
+                });
+            }
+        }
+
+        public void DoClient(TcpClient socket)
+        {
+            using (StreamWriter sw = new StreamWriter(socket.GetStream()))
+            using (StreamReader sr = new StreamReader(socket.GetStream()))
+            {
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+                Car newCar = JsonConvert.DeserializeObject<Car>(line);
+
+                Console.WriteLine(newCar);
+            }
+        }
+    }
+}
